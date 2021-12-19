e73cddc9a685cdb709e0376662e9904df1f87a8f
Nicholas Schroder <nrs4492@hotmail.com>
2021-12-19 12:37:04 -0500

    Added angry boss and table of contents exercise

--- Chapter6.rb
+++ Chapter6.rb
@@ -8,3 +8,30 @@ lastName = gets.chomp
 nameLength = firstName.length+middleName.length+lastName.length
 puts nameLength
 puts 'Did you know that there are ' + nameLength.to_s + ' characters in your name?'
+
+#angry boss
+puts 'Hey, what\'s the matter?! Your productivity has been terrible!!!'
+user_reply = gets.chomp
+puts 'WHADDYA MEAN "' + userReply.capitalize + '"?!?! YOUR\'RE FIRED'
+
+#table of contents
+line_width = 50
+chap_one = 'Chapter 1:'
+chap_two = 'Chapter 2:'
+chap_three = 'Chapter 3:'
+chap_one_title = 'Getting Started'
+chap_two_title = 'Numbers'
+chap_three_title = 'Letters'
+chap_one_pg = 'page  1'
+chap_two_pg = 'page  9'
+chap_three_pg = 'page 13'
+title = 'Table of Contents'
+puts(title.center(line_width))
+puts 
+puts(chap_one.ljust(line_width/3)+chap_one_title.ljust(line_width/3)+chap_one_pg.rjust(line_width/3))
+puts(chap_two.ljust(line_width/3)+chap_two_title.ljust(line_width/3)+chap_two_pg.rjust(line_width/3))
+puts(chap_three.ljust(line_width/3)+chap_three_title.ljust(line_width/3)+chap_three_pg.rjust(line_width/3))
+
+
+
+
