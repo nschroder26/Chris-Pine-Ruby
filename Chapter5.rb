74b1fe1132074c7feca97a121156072c17f111df
Nicholas Schroder <nrs4492@hotmail.com>
2021-12-18 20:24:27 -0500

    Added name requet/return

---
+++ Chapter5.rb
@@ -0,0 +1,7 @@
+puts 'Hello there what\'s your first name?'
+firstName = gets.chomp
+puts 'Thanks ' + firstName + '. What\'s your middle name?'
+middleName = gets.chomp
+puts 'What\'s your last name?'
+lastName = gets.chomp
+puts 'Hi ' + firstName + ' ' + middleName + ' ' + lastName + ' ' + ', nice to meet you'
