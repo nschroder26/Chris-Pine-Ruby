e1a5f5a990adb177d2abe49f2272fe5aefa5bbb3
Nicholas Schroder <nrs4492@hotmail.com>
2021-12-18 21:06:40 -0500

    un-commented section

--- Chapter5.rb
+++ Chapter5.rb
@@ -1,11 +1,11 @@
 #name request/return
-# puts 'Hello there what\'s your first name?'
-# firstName = gets.chomp
-# puts 'Thanks ' + firstName + '. What\'s your middle name?'
-# middleName = gets.chomp
-# puts 'What\'s your last name?'
-# lastName = gets.chomp
-# puts 'Hi ' + firstName + ' ' + middleName + ' ' + lastName +', nice to meet you'
+puts 'Hello there what\'s your first name?'
+firstName = gets.chomp
+puts 'Thanks ' + firstName + '. What\'s your middle name?'
+middleName = gets.chomp
+puts 'What\'s your last name?'
+lastName = gets.chomp
+puts 'Hi ' + firstName + ' ' + middleName + ' ' + lastName +', nice to meet you'
 
 #better number
 puts 'Hey there, what\'s your favorite number?'
