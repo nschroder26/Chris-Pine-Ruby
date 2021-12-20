5eab4830189dd7ada25ec3196ff4a1303d87a2b6
Nicholas Schroder <nrs4492@hotmail.com>
2021-12-20 12:37:44 -0500

    added leap year program

--- Chapter7.rb
+++ Chapter7.rb
@@ -1,14 +1,14 @@
-#99 bottles of beer
-# num_beer = 99
-# while num_beer > 95
-# 	puts num_beer.to_s + ' bottles of beer on the wall, ' + num_beer.to_s + 
-# 	' bottles of beer! Take one down, pass it around! ' + num_beer.to_s + 
-# 	' bottles of beer!!'
-# 	num_beer = num_beer - 1
-# 	if num_beer <= 95
-# 		break
-# 	end
-# end
+#99 bottles of beer. only runs to 95, adjust line 3 and 8 to desired length
+num_beer = 99
+while num_beer > 95
+	puts num_beer.to_s + ' bottles of beer on the wall, ' + num_beer.to_s + 
+	' bottles of beer! Take one down, pass it around! ' + num_beer.to_s + 
+	' bottles of beer!!'
+	num_beer = num_beer - 1
+	if num_beer <= 95
+		break
+	end
+end
 
 #deaf grandma
 said_bye = 0
@@ -32,6 +32,27 @@ while said_bye < 3
 			said_bye = 0
 		end
 end
+
+#leap years
+puts 'Enter a starting year'
+starting_year = gets.chomp
+puts 'Enter an ending year'
+ending_year = gets.chomp
+c_year = starting_year.to_i
+while c_year <= ending_year.to_i
+	if  c_year%100 == 0 && c_year%400 == 0
+		puts c_year
+		c_year += 1
+	elsif c_year%100 == 0
+		c_year += 1
+	elsif c_year%4 == 0
+		puts c_year
+		c_year += 1
+	else
+		c_year += 1
+	end
+end
+
 
 
 
