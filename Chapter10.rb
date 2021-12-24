13e70408a3b0d98da353d08641f875de4d143baf
Nicholas Schroder <nrs4492@hotmail.com>
2021-12-24 11:46:01 -0500

    added word sorting with recursion, doesnt fully work.

--- Chapter10.rb
+++ Chapter10.rb
@@ -1,37 +1,101 @@
 #list sorting no recursion
+# entered_list = ['people', 'tree', 'tree', 'snow', 'cold', 'cookies',
+# 				'gifts']
+# def word_sort list
+# 	sorted_list = []
+# 	unsorted_list = []
+# 	puts list.length
+# 	while list.length>0
+# 		word_one = list.pop
+# 		word_two = list.pop
+# 		if word_two == nil
+# 			sorted_list.push word_one
+# 			list = unsorted_list
+# 			unsorted_list =[]
+# 		elsif word_one < word_two
+# 			unsorted_list.push word_two
+# 			list.push word_one
+# 		elsif word_one == word_two
+# 			list.push word_two
+# 		else
+# 			unsorted_list.push word_one
+# 			list.push word_two
+# 		end
+# 	end
+# 	puts sorted_list
+# end
+
+# word_sort entered_list
+
+# word sorting with recursion (doesnt fully work, only returns first word)
 entered_list = ['people', 'tree', 'tree', 'snow', 'cold', 'cookies',
 				'gifts']
-def word_sort list
-	sorted_list = []
-	unsorted_list = []
-	puts list.length
-	while list.length>0
-		word_one = list.pop
-		word_two = list.pop
-		if word_two == nil
-			sorted_list.push word_one
-			list = unsorted_list
-			unsorted_list =[]
-			# puts '-----'
-			# puts list
-			# puts '-----'
-			# puts list.length
-			# break
-		elsif word_one < word_two
-			# puts word_one+' word one'
-			# puts word_two+' word two'
-			unsorted_list.push word_two
-			list.push word_one
-		elsif word_one == word_two
-			list.push word_two
-		else
-			# puts word_one+' word one'
-			# puts word_two+' word two'
-			unsorted_list.push word_one
-			list.push word_two
-		end
-	end
-	puts sorted_list
+def sort_array some_array
+	recursive_sort some_array, [], []
 end
 
-word_sort entered_list
+def recursive_sort unsorted_list, sorted_list, checked_list
+	# puts '-US-'
+	# puts unsorted_list
+	# puts '--'
+	# puts '-CL-'
+	# puts checked_list
+	# puts '--'
+	if unsorted_list.length > 0
+		word_one = unsorted_list.pop
+		word_two = unsorted_list.pop
+		# puts word_one
+		# puts word_two
+		# puts
+		if word_two == nil
+			sorted_list.push word_one
+		elsif word_two < word_one
+			checked_list.push word_one
+			unsorted_list.push word_two
+		elsif word_one == word_two
+			checked_list.push word_one
+		elsif
+			checked_list.push word_two
+			unsorted_list.push word_one
+		end
+		recursive_sort unsorted_list, [], []
+	end
+	puts sorted_list
+	# puts 
+	# puts checked_list
+end
+
+sort_array entered_list
+
+
+# entered_list = ['people', 'tree', 'tree', 'snow', 'cold', 'cookies',
+# 				'gifts']
+# def sort_array some_array
+# 	recursive_sort some_array, []
+# end
+
+# def recursive_sort unsorted_list, sorted_list
+# 	if unsorted_list.length>1
+# 		word_one = unsorted_list.pop
+# 		word_two = unsorted_list.pop
+# 		if word_two == nil
+# 			sorted_list.push word_one
+# 		elsif word_one < word_two
+# 			unsorted_list.push word_two
+# 		elsif word_one == word_two
+# 			unsorted_list.push word_two
+# 		end
+# 		recursive_sort unsorted_list, sorted_list
+# 	else
+# 		word_one = unsorted_list.pop
+# 		sorted_list.push word_one
+# 	end
+# 	puts sorted_list
+# 	puts unsorted_list
+# end
+
+# sort_array entered_list
+
+
+
+
