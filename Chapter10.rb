1dfda9ee16474c4c6b49eb94bf878848dc89c5fb
Nicholas Schroder <nrs4492@hotmail.com>
2021-12-26 14:11:18 -0500

    added dictionary sorting

--- Chapter10.rb
+++ Chapter10.rb
@@ -102,27 +102,117 @@
 
 
 #array shuffler
-entered_list = ['people', 'tree', 'tree', 'snow', 'cold', 'cookies',
-				'gifts']
-def shuffle_wrapper any_array
-	shuffle_array any_array, []
+# entered_list = ['people', 'tree', 'tree', 'snow', 'cold', 'cookies',
+# 				'gifts']
+# def shuffle_wrapper any_array
+# 	shuffle_array any_array, []
+# end
+
+# def shuffle_array entered_array, shuffled_array
+# 	if entered_array.length == 0
+# 		shuffled_array.reject!{|e| e.to_s.empty?}
+# 		puts shuffled_array
+# 		exit!
+# 	else
+# 		shuffled_word = entered_array.pop
+# 		shuffled_array.insert(rand(entered_array.length+shuffled_array.length), shuffled_word)
+# 	end
+# 	shuffle_array entered_array, shuffled_array
+# end
+
+# shuffle_wrapper entered_list
+
+#dictionary word sorting
+entered_list = ['people', 'tree', 'tree', 'Snow', 'cold', 'cookies',
+				'Gifts', 'ice', 'ice']
+def sort_array some_array
+	recursive_sort some_array, [], []
 end
 
-def shuffle_array entered_array, shuffled_array
-	if entered_array.length == 0
-		shuffled_array.reject!{|e| e.to_s.empty?}
-		puts shuffled_array
+def recursive_sort unsorted_list, sorted_list, checked_list
+	if unsorted_list.length == 0
+		puts sorted_list
 		exit!
-	else
-		shuffled_word = entered_array.pop
-		shuffled_array.insert(rand(entered_array.length+shuffled_array.length), shuffled_word)
 	end
-	shuffle_array entered_array, shuffled_array
+	word_one = unsorted_list.pop
+	word_two = unsorted_list.pop
+	word_one_cap = false
+	word_two_cap = false
+	# puts word_one
+	# puts word_two
+	# puts '--'
+
+	if word_one == word_one.capitalize
+		word_one_cap = true
+		word_one = word_one.downcase
+	end
+	if word_two != nil && word_two == word_two.capitalize
+		word_two_cap = true
+		word_two = word_two.downcase
+	end
+	if word_two == nil
+		if word_one_cap
+			word_one = word_one.capitalize
+			sorted_list.push word_one
+			word_one_cap = false
+			unsorted_list = checked_list
+			checked_list = []
+		else
+			sorted_list.push word_one
+			unsorted_list = checked_list
+			checked_list = []
+		end
+	elsif word_two < word_one
+		if word_one_cap && word_two_cap
+			word_one = word_one.capitalize
+			word_two = word_two.capitalize
+			unsorted_list.push word_two
+			word_one_cap = false
+			word_two_cap = false
+			checked_list.push word_one
+		elsif word_one_cap
+			word_one = word_one.capitalize
+			unsorted_list.push word_two
+			word_one_cap = false
+			checked_list.push word_one
+		elsif word_two_cap
+			word_two = word_two.capitalize
+			unsorted_list.push word_two
+			word_two_cap = false
+			checked_list.push word_one
+		else
+			unsorted_list.push word_two
+			checked_list.push word_one
+		end
+	elsif word_one == word_two
+		unsorted_list.push word_one
+	elsif word_one < word_two
+		if word_one_cap && word_two_cap
+			word_one = word_one.capitalize
+			word_two = word_two.capitalize
+			unsorted_list.push word_one
+			word_one_cap = false
+			word_two_cap = false
+			checked_list.push word_two
+		elsif word_one_cap
+			word_one = word_one.capitalize
+			unsorted_list.push word_one
+			word_one_cap = false
+			checked_list.push word_two
+		elsif word_two_cap
+			word_two = word_two.capitalize
+			unsorted_list.push word_one
+			word_two_cap = false
+			checked_list.push word_two
+		else
+			unsorted_list.push word_one
+			checked_list.push word_two
+		end
+	end
+	recursive_sort unsorted_list, sorted_list, checked_list
 end
 
-shuffle_wrapper entered_list
-
-
+sort_array entered_list
 
 
 
