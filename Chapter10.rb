9da46e590ed705ea905ad756555a5798dffface7
Nicholas Schroder <nrs4492@hotmail.com>
2021-12-27 13:10:52 -0500

    created wedding numbers with and statement

--- Chapter10.rb
+++ Chapter10.rb
@@ -212,7 +212,250 @@
 
 
 #english numbers
-def english_number number
+# def english_number number
+# 	if number < 0
+# 		return 'Please enter a number that isn\'t negative.' 
+# 	end
+# 	if number == 0
+# 		return 'zero'
+# 	end
+# 	num_string = ''
+
+# 	ones_place = ['one', 'two', 'three', 'four', 'five',
+# 					'six', 'seven', 'eight', 'nine']
+# 	tens_place = ['ten', 'twenty', 'thirty', 'fourty', 'fifty',
+# 					'sixty', 'seventy', 'eighty', 'ninety']
+# 	teenagers = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen',
+# 					'sixteen', 'seventeen', 'eighteen', 'nineteen']
+
+# 	left = number
+
+# 	#trillions
+# 	write = left/100000000000000
+# 	left = left - write*100000000000000
+# 	if write > 0
+# 		hund_thousands = english_number write
+# 		if left > 100000000000000
+# 			num_string = num_string + hund_thousands + ' hundred trillion'
+# 		elsif left < 1000000000000
+# 			num_string = num_string + hund_thousands + ' hundred trillion'
+# 		else	
+# 			num_string = num_string + hund_thousands + ' hundred'
+# 		end
+# 		if left > 0
+# 			num_string = num_string +' '
+# 		end
+# 	end
+
+# 	write = left/10000000000000
+# 	left = left - write*10000000000000
+# 	if write > 0
+# 		if ((write == 1) and (left > 1000000000000))
+# 			num_string = num_string + teenagers[(left/1000000000000)-1] + ' trillion'
+# 			left = left%1000000000000
+# 		elsif left < 1000000000000
+# 			num_string = num_string + tens_place[write-1] + ' trillion'
+			
+# 		else
+# 			num_string = num_string + tens_place[write-1]
+# 		end
+# 		if left > 1000000000000
+# 			num_string = num_string + '-'
+# 		else
+# 			num_string = num_string + ' '
+# 		end 
+# 	end
+
+# 	write = left/1000000000000
+# 	left = left - write*1000000000000
+# 	if write > 0
+# 		millions = english_number write
+# 		num_string = num_string + millions + ' trillion'
+# 		if left > 0
+# 			num_string = num_string +' '
+# 		end
+# 	end
+
+# 	#billions
+# 	write = left/100000000000
+# 	left = left - write*100000000000
+# 	if write > 0
+# 		hund_thousands = english_number write
+# 		if left > 100000000000
+# 			num_string = num_string + hund_thousands + ' hundred billion'
+# 		elsif left < 1000000000
+# 			num_string = num_string + hund_thousands + ' hundred billion'
+# 		else	
+# 			num_string = num_string + hund_thousands + ' hundred'
+# 		end
+# 		if left > 0
+# 			num_string = num_string +' '
+# 		end
+# 	end
+
+# 	write = left/10000000000
+# 	left = left - write*10000000000
+# 	if write > 0
+# 		if ((write == 1) and (left > 1000000000))
+# 			num_string = num_string + teenagers[(left/1000000000)-1] + ' billion'
+# 			left = left%1000000000
+# 		elsif left < 1000000000
+# 			num_string = num_string + tens_place[write-1] + ' billion'
+			
+# 		else
+# 			num_string = num_string + tens_place[write-1]
+# 		end
+# 		if left > 1000000000
+# 			num_string = num_string + '-'
+# 		else
+# 			num_string = num_string + ' '
+# 		end 
+# 	end
+
+# 	write = left/1000000000
+# 	left = left - write*1000000000
+# 	if write > 0
+# 		millions = english_number write
+# 		num_string = num_string + millions + ' billion'
+# 		if left > 0
+# 			num_string = num_string +' '
+# 		end
+# 	end
+
+# 	#millions
+# 	write = left/100000000
+# 	left = left - write*100000000
+# 	if write > 0
+# 		hund_thousands = english_number write
+# 		if left > 100000000
+# 			num_string = num_string + hund_thousands + ' hundred million'
+# 		elsif left < 1000000
+# 			num_string = num_string + hund_thousands + ' hundred million'
+# 		else	
+# 			num_string = num_string + hund_thousands + ' hundred'
+# 		end
+# 		if left > 0
+# 			num_string = num_string +' '
+# 		end
+# 	end
+
+# 	write = left/10000000
+# 	left = left - write*10000000
+# 	if write > 0
+# 		if ((write == 1) and (left > 1000000))
+# 			num_string = num_string + teenagers[(left/1000000)-1] + ' million'
+# 			left = left%1000000
+# 		elsif left < 1000000
+# 			num_string = num_string + tens_place[write-1] + ' million'
+			
+# 		else
+# 			num_string = num_string + tens_place[write-1]
+# 		end
+# 		if left > 1000000
+# 			num_string = num_string + '-'
+# 		else
+# 			num_string = num_string + ' '
+# 		end 
+# 	end
+
+# 	write = left/1000000
+# 	left = left - write*1000000
+# 	if write > 0
+# 		millions = english_number write
+# 		num_string = num_string + millions + ' million'
+# 		if left > 0
+# 			num_string = num_string +' '
+# 		end
+# 	end
+
+# 	#thousands
+# 	write = left/100000
+# 	left = left - write*100000
+# 	if write > 0
+# 		hund_thousands = english_number write
+# 		if left > 100000
+# 			num_string = num_string + hund_thousands + ' hundred thousand'
+# 		elsif left < 1000
+# 			num_string = num_string + hund_thousands + ' hundred thousand'
+# 		else	
+# 			num_string = num_string + hund_thousands + ' hundred'
+# 		end
+# 		if left > 0
+# 			num_string = num_string +' '
+# 		end
+# 	end
+
+# 	write = left/10000
+# 	left = left - write*10000
+# 	if write > 0
+# 		if ((write == 1) and (left > 1000))
+# 			num_string = num_string + teenagers[(left/1000)-1] + ' thousand'
+# 			left = left%1000
+# 		elsif left < 1000
+# 			num_string = num_string + tens_place[write-1] + ' thousand'
+			
+# 		else
+# 			num_string = num_string + tens_place[write-1]
+# 		end
+# 		if left > 1000
+# 			num_string = num_string + '-'
+# 		else
+# 			num_string = num_string + ' '
+# 		end 
+# 	end
+
+# 	write = left/1000
+# 	left = left - write*1000
+# 	if write > 0
+# 		thousands = english_number write
+# 		num_string = num_string + thousands + ' thousand'
+# 		if left > 0
+# 			num_string = num_string +' '
+# 		end
+# 	end
+
+# 	#hundreds
+# 	write = left/100
+# 	left = left - write*100
+# 	if write > 0
+# 		hundreds = english_number write
+# 		num_string = num_string + hundreds + ' hundred'
+# 		if left > 0
+# 			num_string = num_string +' '
+# 		end
+# 	end
+
+# 	write = left/10
+# 	left = left - write*10
+# 	if write > 0
+# 		if ((write == 1) and (left > 0))
+# 			num_string = num_string + teenagers[left-1]
+# 			left = 0
+# 		else
+# 			num_string = num_string + tens_place[write-1]
+# 		end
+# 		if left > 0
+# 				num_string = num_string + '-'
+# 		end 
+# 	end
+# 	write = left
+# 	left = 0
+
+# 	if write > 0
+# 		num_string = num_string + ones_place[write-1]
+# 	end
+# 	num_string
+# end
+
+
+# # puts english_number(3211)
+# # puts english_number(10012)
+# # puts english_number(817009)
+# puts english_number(756876100000870)
+
+
+#wedding number
+def wedding_number number
 	if number < 0
 		return 'Please enter a number that isn\'t negative.' 
 	end
@@ -230,145 +473,11 @@ def english_number number
 
 	left = number
 
-	write = left/100000000000000
-	left = left - write*100000000000000
-	if write > 0
-		hund_thousands = english_number write
-		if left > 100000000000000
-			num_string = num_string + hund_thousands + ' hundred trillion'
-		elsif left < 1000000000000
-			num_string = num_string + hund_thousands + ' hundred trillion'
-		else	
-			num_string = num_string + hund_thousands + ' hundred'
-		end
-		if left > 0
-			num_string = num_string +' '
-		end
-	end
-
-	write = left/10000000000000
-	left = left - write*10000000000000
-	if write > 0
-		if ((write == 1) and (left > 1000000000000))
-			num_string = num_string + teenagers[(left/1000000000000)-1] + ' trillion'
-			left = left%1000000000000
-		elsif left < 1000000000000
-			num_string = num_string + tens_place[write-1] + ' trillion'
-			
-		else
-			num_string = num_string + tens_place[write-1]
-		end
-		if left > 1000000000000
-			num_string = num_string + '-'
-		else
-			num_string = num_string + ' '
-		end 
-	end
-
-	write = left/1000000000000
-	left = left - write*1000000000000
-	if write > 0
-		millions = english_number write
-		num_string = num_string + millions + ' trillion'
-		if left > 0
-			num_string = num_string +' '
-		end
-	end
-
-	write = left/100000000000
-	left = left - write*100000000000
-	if write > 0
-		hund_thousands = english_number write
-		if left > 100000000000
-			num_string = num_string + hund_thousands + ' hundred billion'
-		elsif left < 1000000000
-			num_string = num_string + hund_thousands + ' hundred billion'
-		else	
-			num_string = num_string + hund_thousands + ' hundred'
-		end
-		if left > 0
-			num_string = num_string +' '
-		end
-	end
-
-	write = left/10000000000
-	left = left - write*10000000000
-	if write > 0
-		if ((write == 1) and (left > 1000000000))
-			num_string = num_string + teenagers[(left/1000000000)-1] + ' billion'
-			left = left%1000000000
-		elsif left < 1000000000
-			num_string = num_string + tens_place[write-1] + ' billion'
-			
-		else
-			num_string = num_string + tens_place[write-1]
-		end
-		if left > 1000000000
-			num_string = num_string + '-'
-		else
-			num_string = num_string + ' '
-		end 
-	end
-
-	write = left/1000000000
-	left = left - write*1000000000
-	if write > 0
-		millions = english_number write
-		num_string = num_string + millions + ' billion'
-		if left > 0
-			num_string = num_string +' '
-		end
-	end
-
-	write = left/100000000
-	left = left - write*100000000
-	if write > 0
-		hund_thousands = english_number write
-		if left > 100000000
-			num_string = num_string + hund_thousands + ' hundred million'
-		elsif left < 1000000
-			num_string = num_string + hund_thousands + ' hundred million'
-		else	
-			num_string = num_string + hund_thousands + ' hundred'
-		end
-		if left > 0
-			num_string = num_string +' '
-		end
-	end
-
-	write = left/10000000
-	left = left - write*10000000
-	if write > 0
-		if ((write == 1) and (left > 1000000))
-			num_string = num_string + teenagers[(left/1000000)-1] + ' million'
-			left = left%1000000
-		elsif left < 1000000
-			num_string = num_string + tens_place[write-1] + ' million'
-			
-		else
-			num_string = num_string + tens_place[write-1]
-		end
-		if left > 1000000
-			num_string = num_string + '-'
-		else
-			num_string = num_string + ' '
-		end 
-	end
-
-	write = left/1000000
-	left = left - write*1000000
-	if write > 0
-		millions = english_number write
-		num_string = num_string + millions + ' million'
-		if left > 0
-			num_string = num_string +' '
-		end
-	end
-
+	#thousands
 	write = left/100000
 	left = left - write*100000
 	if write > 0
-		hund_thousands = english_number write
+		hund_thousands = wedding_number write
 		if left > 100000
 			num_string = num_string + hund_thousands + ' hundred thousand'
 		elsif left < 1000
@@ -403,18 +512,24 @@ def english_number number
 	write = left/1000
 	left = left - write*1000
 	if write > 0
-		thousands = english_number write
+		thousands = wedding_number write
 		num_string = num_string + thousands + ' thousand'
 		if left > 0
 			num_string = num_string +' '
 		end
 	end
 
-
+	#hundreds
 	write = left/100
 	left = left - write*100
-	if write > 0
-		hundreds = english_number write
+	puts
+	puts write
+	puts left
+	if write > 0 && left > 0
+		hundreds = wedding_number write
+		num_string = num_string + hundreds + ' hundred and '
+	elsif write > 0
+		hundreds = wedding_number write
 		num_string = num_string + hundreds + ' hundred'
 		if left > 0
 			num_string = num_string +' '
@@ -443,18 +558,7 @@ def english_number number
 	num_string
 end
 
-
-# puts english_number(3211)
-# puts english_number(10012)
-# puts english_number(817009)
-puts english_number(756876100000870)
-
-
-
-
-
-
-
+puts wedding_number(1901)
 
 
 
