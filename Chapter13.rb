e277d9f1b09d06e72b3fb6a3882ec572673d187a
Nicholas Schroder <nrs4492@hotmail.com>
2021-12-30 10:26:18 -0500

    expanded 3 classes

---
+++ Chapter13.rb
@@ -0,0 +1,103 @@
+#extending classes
+
+# class Array
+# 	def shuffle
+# 		entered_array = self
+# 		shuffled_array = []
+
+# 		while entered_array.length > 0
+# 			rand_entry = rand(entered_array.length)
+# 			starting_index = 0
+# 			nonshuf_array = []
+
+# 			entered_array.each do |entry|
+# 				if starting_index == rand_entry
+# 					shuffled_array.push entry
+# 				else
+# 					nonshuf_array.push entry
+# 				end
+# 				starting_index += 1
+# 			end
+# 			entered_array = nonshuf_array
+# 		end
+# 		shuffled_array
+# 	end
+# end
+
+# puts [1,2,3,4,5,6].shuffle
+
+# class Integer
+# 	def factorial
+# 		user_entry = self
+# 		solution = user_entry
+# 		if user_entry <1
+# 			return 'Please enter a valid non-negative number'
+# 		else
+# 			if user_entry == 1
+# 				return 1
+# 			else
+# 				while user_entry > 1
+# 					solution = solution *(user_entry-1)
+# 					user_entry -= 1
+# 				end
+# 				puts solution
+# 			end
+# 		end
+# 	end
+# end
+
+# puts 6.factorial
+
+
+class Integer
+	def to_roman
+		user_entry = self
+		roman_num = ''
+		while user_entry > 0
+			if user_entry >= 1000
+				roman_num += 'M' * (user_entry/1000)
+				user_entry = user_entry%1000
+			elsif user_entry >= 500
+				roman_num += 'D' * (user_entry/500)
+				user_entry = user_entry%500
+			elsif user_entry >= 100
+				roman_num += 'C' * (user_entry/100)
+				user_entry = user_entry%100
+			elsif user_entry >= 50
+				roman_num += 'L' * (user_entry/50)
+				user_entry = user_entry%50
+			elsif user_entry >= 10
+				roman_num += 'X' * (user_entry/10)
+				user_entry = user_entry%10
+			elsif user_entry >= 5
+				roman_num += 'V' * (user_entry/5)
+				user_entry = user_entry%5
+			elsif user_entry >= 1
+				roman_num += 'I' * (user_entry/1)
+				user_entry = user_entry%1
+			else
+				puts 'Please enter a valid number to convert'
+				exit
+			end
+		end
+		puts roman_num
+	end
+end
+
+puts 12.to_roman
+puts 56.to_roman
+puts 134.to_roman
+puts 578.to_roman
+puts 1978.to_roman
+				
+
+
+
+
+
+
+
+
+
+
+
