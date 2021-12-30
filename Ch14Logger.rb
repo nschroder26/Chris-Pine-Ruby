962e4976fc3ea89081de0f2b0c3489e538e7ac01
Nicholas Schroder <nrs4492@hotmail.com>
2021-12-30 15:22:52 -0500

    basic logger

---
+++ Ch14Logger.rb
@@ -0,0 +1,22 @@
+def prog_logger block_desc, &block
+	puts "Beginning#{block_desc}"
+	block.call
+end
+
+# small_block = Proc.new do
+# 	some_number = rand(7)
+# 	puts '"small block" finished, returning:'"#{some_number}"
+# end
+
+prog_logger ' "outer block"...' do
+	prog_logger '"small block"...' do
+		some_number = rand(7)
+		puts '..."small block" finished, returning:'"	#{some_number}"
+	end
+	prog_logger '"another small block"...' do
+		output = 'this is the last chapter'
+		puts '..."small block" finished, returning:'"	#{output}"
+	end
+	a = false
+	puts '..."outer block" finished, returning:'"	#{a}"
+end
